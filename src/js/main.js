ccb5380545bc0c8809bf744ab39ac9e09243fb4f
Otsel <sonderaaron@gmail.com>
2024-07-28 15:54:16 -0400

    added status readout for player actions

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,49 @@ $('.card').each(function(){
     }, 1500);
 });
 
+
+//UI actions
+
+//count number of status messages
+var statusCounter = 1;
+
+//draw 7
+$('#menuItem2').on('click', function(){
+    $('#defaultStatus').remove();
+    var statusID = "status"+statusCounter;
+    var statusTarget ="#status"+(statusCounter-1);
+    var statusMessage = "<div class='status' "+"id='"+statusID+"'>you drew 7 cards</div>";
+    console.log(statusMessage);
+    console.log(statusTarget);
+    if (statusCounter == 5) {
+        $(".status").remove();
+        $('.statusContainer').prepend(statusMessage);
+        statusCounter = 1;
+    } else {
+        $('.statusContainer').prepend(statusMessage);
+        statusCounter++;
+    }
+    
+});
+
+//draw 1
+$('#menuItem3').on('click', function(){
+    $('#defaultStatus').remove();
+    var statusID = "status"+statusCounter;
+    var statusTarget ="#status"+(statusCounter-1);
+    var statusMessage = "<div class='status' "+"id='"+statusID+"'>you drew 1 card</div>";
+    console.log(statusMessage);
+    console.log(statusTarget);
+    if (statusCounter == 5) {
+        $(".status").remove();
+        $('.statusContainer').prepend(statusMessage);
+        statusCounter = 1;
+    } else {
+        $('.statusContainer').prepend(statusMessage);
+        statusCounter++;
+    }
+    
+});
 //make cards draggable
 
 //drop the card on release
@@ -38,11 +81,8 @@ $('.playerHand').droppable({
 
 // tooltips
 $('.menuItem').hover(function(){
-    console.log("menu item hovered");
     var menuItemID = $(this).attr("id");
-    console.log(menuItemID)
     var tooltipID = "[aria-controls='" + menuItemID + "']";
-    console.log(tooltipID);
     $(tooltipID).css("opacity", "1");
     $(this).on("mouseleave", function(){
         $('.tooltip').css("opacity", "0");
