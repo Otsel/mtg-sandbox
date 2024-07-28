38a44576ddc603293946947ef6fa14410c9e3c37
Otsel <sonderaaron@gmail.com>
2024-07-28 14:38:13 -0400

    added topmenu tooltips

--- src/js/main.js
+++ src/js/main.js
@@ -19,14 +19,36 @@ $('.card').each(function(){
 //     $(this).css("height", "200px");
 // });
 
-$('.playingCard').draggable({containment:"body", scroll: false});
+$('.playingCard').draggable({
+    containment:"body", 
+    scroll: false,
+    drag: function (event, ui) {
+        $(this).toggleClass( "in-hand" );
+        $(this).css("z-index", "1");
+    }
+});
 // $('.playingCard').draggable();
 $('.playerHand').droppable({
     // accept: ".playingCard"
     drop: function( event, ui ) {
-        $(this).toggleClass( "in-hand" );
-        console.log("plop");
+        // $(this).toggleClass( "in-hand" );
+        console.log("plopped in hand");
     }
 });
+
+// tooltips
+$('.menuItem').hover(function(){
+    console.log("menu item hovered");
+    var menuItemID = $(this).attr("id");
+    console.log(menuItemID)
+    var tooltipID = "[aria-controls='" + menuItemID + "']";
+    console.log(tooltipID);
+    $(tooltipID).css("opacity", "1");
+    $(this).on("mouseleave", function(){
+        $('.tooltip').css("opacity", "0");
+    });
+});
+
+
 
 })(jQuery);
